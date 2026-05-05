3b1c2b7a37ebc2c36be25f39ecd9c9a406c63c83
Mael Lebrun <malebrun@3A2.42angouleme.fr>
2026-05-05 06:50:16 +0200

    norme

--- srcs/doors.c
+++ srcs/doors.c
@@ -6,15 +6,15 @@
 /*   By: edi-maio <edi-maio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 01:36:40 by malebrun          #+#    #+#             */
-/*   Updated: 2026/05/05 06:03:00 by edi-maio         ###   ########.fr       */
+/*   Updated: 2026/05/05 06:40:28 by malebrun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Cube3D.h"
 
-static t_rgb    create_rrgb( void )
+static t_rgb	create_rrgb(void)
 {
-	t_rgb   rvalue;
+	t_rgb	rvalue;
 
 	rvalue.red = rrint() % 256;
 	rvalue.green = rrint() % 256;
@@ -22,77 +22,74 @@ static t_rgb    create_rrgb( void )
 	return (rvalue);
 }
 
-static t_door  createdoor(int y, int x)
+static t_door	createdoor(int y, int x)
 {
-    t_door  rvalue;
+	t_door	rvalue;
 
-    rvalue.y = y;
-    rvalue.x = x;
-    rvalue.color = create_rrgb();
-    return (rvalue);
+	rvalue.y = y;
+	rvalue.x = x;
+	rvalue.color = create_rrgb();
+	return (rvalue);
 }
 
-void    init_doors(t_game *game)
+void	init_doors(t_game *game)
 {
-    int i;
-    int j;
-    int count;
-    int k;
+	int	i;
+	int	j;
+	int	k;
 
-    count = 0;
-    i = -1;
-    while(game->map[++i])
-    {
-        j = -1;
-        while(game->map[i][++j])
-            if (game->map[i][j] == 'D')
-                count++;
-    }
-    game->door_count = count;
-    game->doors = malloc(sizeof(t_door) * count);
-    if (!game->doors)
-        return ;
-    i = -1;
-    k = 0;
-    while(game->map[++i])
-    {
-        j = -1;
-        while(game->map[i][++j])
-            if (game->map[i][j] == 'D')
-                game->doors[k++] = createdoor(i, j);
-    }
+	game->door_count = 0;
+	i = -1;
+	while (game->map[++i])
+	{
+		j = -1;
+		while (game->map[i][++j])
+			game->door_count += game->map[i][j] == 'D';
+	}
+	game->doors = malloc(sizeof(t_door) * game->door_count);
+	if (!game->doors)
+		return ;
+	i = -1;
+	k = 0;
+	while (game->map[++i])
+	{
+		j = -1;
+		while (game->map[i][++j])
+			if (game->map[i][j] == 'D')
+				game->doors[k++] = createdoor(i, j);
+	}
 }
 
-static int is_similar(t_rgb a, t_rgb b)
+static int	is_similar(t_rgb a, t_rgb b)
 {
-    int dr;
-    int dg;
-    int db;
+	int	dr;
+	int	dg;
+	int	db;
 
-    dr = a.red - b.red;
-    dg = a.green - b.green;
-    db = a.blue - b.blue;
-    return ((dr * dr + dg * dg + db * db) < (67 * 67));
+	dr = a.red - b.red;
+	dg = a.green - b.green;
+	db = a.blue - b.blue;
+	return ((dr * dr + dg * dg + db * db) < (67 * 67));
 }
 
-void    open_doors(t_game *game)
+void	open_doors(t_game *game)
 {
-    int     i;
-    float   dx;
-    float   dy;
+	int		i;
+	float	dx;
+	float	dy;
 
-    i = 0;
-    while (i < game->door_count)
-    {
-        dx = game->x - game->doors[i].x;
-        dy = game->y - game->doors[i].y;
-        if ((dx * dx + dy * dy) <= 8.0f
-                && is_similar(game->doors[i].color, game->ccolor))
-        {
-            game->map[game->doors[i].y][game->doors[i].x] = '0';
-            game->doors[i].x = -1;
-            game->doors[i].y = -1;
-        }
-        i++;
-    }
+	i = 0;
+	while (i < game->door_count)
+	{
+		dx = game->x - game->doors[i].x;
+		dy = game->y - game->doors[i].y;
+		if ((dx * dx + dy * dy) <= 8.0f
+			&& is_similar(game->doors[i].color, game->ccolor))
+		{
+			game->map[game->doors[i].y][game->doors[i].x] = '0';
+			game->doors[i].x = -1;
+			game->doors[i].y = -1;
+		}
+		i++;
+	}
 }
--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,98 +6,96 @@
 /*   By: edi-maio <edi-maio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 01:55:58 by edi-maio          #+#    #+#             */
-/*   Updated: 2026/05/05 04:07:42 by edi-maio         ###   ########.fr       */
+/*   Updated: 2026/05/05 06:49:52 by malebrun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Cube3D.h"
 
-static void    draw_minimap_cell(t_game *game, int x, int y, int color)
+static void	draw_minimap_cell(t_game *game, int x, int y, int color)
 {
-    int    i;
-    int    j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (i < 10)
-    {
-        j = 0;
-        while (j < 10)
-            my_mlx_pixel_put(game, x + j++, y + i, color);
-        i++;
-    }
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+			my_mlx_pixel_put(game, x + j++, y + i, color);
+		i++;
+	}
 }
 
-static void    draw_minimap_tiles(t_game *game, int ox, int oy)
+static void	draw_minimap_tiles(t_game *game, int ox, int oy)
 {
-    int    i;
-    int    j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (i < MM_SIZE && game->map[oy + i])
-    {
-        j = 0;
-        while (j < MM_SIZE && game->map[oy + i][ox + j])
-        {
-            if (game->map[oy + i][ox + j] == '1')
-                draw_minimap_cell(game, j * 10, i * 10, 0x000000);
-            else if (game->map[oy + i][ox + j] == 'D')
-                draw_minimap_cell(game, j * 10, i * 10,
-                    get_door_color(game, oy + i, ox + j));
-            else
-                draw_minimap_cell(game, j * 10, i * 10, 0xFFFFFF);
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (i < MM_SIZE && game->map[oy + i])
+	{
+		j = 0;
+		while (j < MM_SIZE && game->map[oy + i][ox + j])
+		{
+			if (game->map[oy + i][ox + j] == '1')
+				draw_minimap_cell(game, j * 10, i * 10, 0x000000);
+			else if (game->map[oy + i][ox + j] == 'D')
+				draw_minimap_cell(game, j * 10, i * 10,
+					get_door_color(game, oy + i, ox + j));
+			else
+				draw_minimap_cell(game, j * 10, i * 10, 0xFFFFFF);
+			j++;
+		}
+		i++;
+	}
 }
 
-static void    draw_ray(t_game *game, int px, int py, float angle)
+static void	draw_ray(t_game *game, int px, int py, float angle)
 {
-    int    k;
+	int	k;
 
-    k = 0;
-    while (k < 20)
-    {
-        my_mlx_pixel_put(game,
-            px + (int)(cosf(angle) * k),
-            py + (int)(sinf(angle) * k),
-            0x00FF00);
-        k++;
-    }
+	k = 0;
+	while (k < 20)
+	{
+		my_mlx_pixel_put(game, px + (int)(cosf(angle) * k),
+			py + (int)(sinf(angle) * k), 0x00FF00);
+		k++;
+	}
 }
 
-static void    draw_player(t_game *game, int px, int py)
+static void	draw_player(t_game *game, int px, int py)
 {
-    float    angle;
-    int      i;
-    int      j;
+	float	angle;
+	int		i;
+	int		j;
 
-    angle = atan2f(game->dy, game->dx);
-    i = -2;
-    while (i <= 2)
-    {
-        j = -2;
-        while (j <= 2)
-            my_mlx_pixel_put(game, px + j++, py + i, 0xFF0000);
-        i++;
-    }
-    draw_ray(game, px, py, angle - 0.5236f);
-    draw_ray(game, px, py, angle + 0.5236f);
+	angle = atan2f(game->dy, game->dx);
+	i = -2;
+	while (i <= 2)
+	{
+		j = -2;
+		while (j <= 2)
+			my_mlx_pixel_put(game, px + j++, py + i, 0xFF0000);
+		i++;
+	}
+	draw_ray(game, px, py, angle - 0.5236f);
+	draw_ray(game, px, py, angle + 0.5236f);
 }
 
-void    display_minimap(t_game *game)
+void	display_minimap(t_game *game)
 {
-    int    ox;
-    int    oy;
+	int	ox;
+	int	oy;
 
-    ox = (int)game->x - MM_SIZE / 2;
-    oy = (int)game->y - MM_SIZE / 2;
-    if (ox < 0)
-        ox = 0;
-    if (oy < 0)
-        oy = 0;
-    draw_minimap_tiles(game, ox, oy);
-    draw_player(game,
-        (int)((game->x - ox) * 10),
-        (int)((game->y - oy) * 10));
+	ox = (int)game->x - MM_SIZE / 2;
+	oy = (int)game->y - MM_SIZE / 2;
+	if (ox < 0)
+		ox = 0;
+	if (oy < 0)
+		oy = 0;
+	draw_minimap_tiles(game, ox, oy);
+	draw_player(game,
+		(int)((game->x - ox) * 10),
+		(int)((game->y - oy) * 10));
 }
